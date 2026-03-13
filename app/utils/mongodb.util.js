307287fe1484b2638abe84f37f66f6d86981cccc
Nguyen Minh Tri <Nguyenminhtri2475@gmail.com>
2026-03-14 06:03:16 +0700

    cai dat cac handler truy xuat csdl

--- app/utils/mongodb.util.js
+++ app/utils/mongodb.util.js
@@ -1,9 +1,11 @@
-const { MongoCLient } = require('mongodb')
+const { MongoClient } = require('mongodb')
 
 class MongoDB {
     static connect = async (uri) => {
         if (this.client) return this.client;
-        this.client = await MongoCLient.connect(uri);
+        this.client = await MongoClient.connect(uri);
         return this.client;
     }
 }
+
+module.exports = MongoDB;
